8050d333d1950cefd44860036ec0ac05e9401a6a
ldv432 <ldv432@gmail.com>
2024-11-18 15:54:29 -0700

    routed my life away

--- server/app.py
+++ server/app.py
@@ -4,5 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<parameter>')
+def print_route(parameter):
+    print(parameter)
+    return f"{parameter}"
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    result = "\n".join(str(i) for i in range(parameter)) + '\n'
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return "Error: Division by zero isn't allowed.", 400
+        result = num1 / num2
+    elif operation == '%':
+        if num2 == 0:
+            return "Error: Modulo by zero is not allowed.", 400
+        result = num1 % num2
+    else:
+        return "Error: Unsupported operation.", 400
+    return str(result)
+   
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
